47369c35405531e8b9ec2dcff704808f14495dcc
Ivan Baena <ivan.baena04@gmail.com>
2016-09-27 14:20:43 -0400

    templated home page

--- client/app/partials/grid.jsx
+++ client/app/partials/grid.jsx
@@ -20,31 +20,44 @@ export default class Grid extends TrackerReact(Component) {
 
   render() {
     return (
-      <div id="product-grid" className="row">
-        <div className="first-catalog">
-            <h4 id="first-title">Catalog Title</h4>
+      <div id="product-grid" >
+        <div className="first-catalog row">
+            <h4 id="first-title"> Featured Catalog Title</h4>
           <div className="col s12">
             <div className="media-box">
-            <div className="carousel">
-  <a className="carousel-item" href="#one!"><img src="http://placehold.it/200x200" /></a>
-  <a className="carousel-item" href="#two!"><img src="http://placehold.it/200x200" /></a>
-  <a className="carousel-item" href="#three!"><img src="http://placehold.it/200x200" /></a>
-  <a className="carousel-item" href="#four!"><img src="http://placehold.it/200x200" /></a>
-  <a className="carousel-item" href="#five!"><img src="http://placehold.it/200x200" /></a>
-</div>
+              <div className="carousel">
+                <a className="carousel-item" href="#one!"><img src="http://placehold.it/200x200" /></a>
+                <a className="carousel-item" href="#two!"><img src="http://placehold.it/200x200" /></a>
+                <a className="carousel-item" href="#three!"><img src="http://placehold.it/200x200" /></a>
+                <a className="carousel-item" href="#four!"><img src="http://placehold.it/200x200" /></a>
+                <a className="carousel-item" href="#five!"><img src="http://placehold.it/200x200" /></a>
+              </div>
             </div>
           </div>
         </div>
-        <div className="second-catalog">
-        <h4>Catalog Title</h4>
+        <div className="second-catalog row">
+        <h4> Second Catalog Title</h4>
           <div className="col s6">
             <div className="media-box">
-              <a href="#!"><img src="http://placehold.it/350x150" /></a>
+              <a href="#!"><img src="http://placehold.it/200x200" /></a>
               </div>
           </div>
           <div className="col s6">
             <div className="media-box">
-              <a href="#!"><img src="http://placehold.it/350x150" /></a>
+              <a href="#!"><img src="http://placehold.it/200x200" /></a>
+              </div>
+          </div>
+        </div>
+        <div className="third-catalog row">
+        <h4> Third Catalog Title</h4>
+          <div className="col s6">
+            <div className="media-box">
+              <a href="#!"><img src="http://placehold.it/200x200" /></a>
+              </div>
+          </div>
+          <div className="col s6">
+            <div className="media-box">
+              <a href="#!"><img src="http://placehold.it/200x200" /></a>
               </div>
           </div>
         </div>
